8cb3eb789ba7d3322b33abe321adae7bfe1910b2
iiriszhao <143477473+iiriszhao@users.noreply.github.com>
2023-08-31 14:29:28 +0800

    commit

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 public class Dateable {
-
     public static void main(String[] args) {
-        
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter your age:");
+            int age = Integer.parseInt(scanner.nextLine());
+            System.out.println(age + " year olds should date somebody who is at least " + (7 + (age/2)) + " years old.");
+
+
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 public class Pages {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = Integer.parseInt(scanner.nextLine());
+        System.out.println(age + " year olds should read at least " + (100 - age) + " pages before giving up on a book.");
 
     }
 }
